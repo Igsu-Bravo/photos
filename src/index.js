d8c9e5581963fc732a367d5b8434cceb42869403
Igsu-Bravo <juanbravozuniga@gmail.com>
2019-09-17 21:37:04 +0300

    Initial commit :tada:

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(_=>{});
+
+class _App extends React.Component {
+    render () {
+        return (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
